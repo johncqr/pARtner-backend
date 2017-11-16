63be7298ea43e9eecb9d06cf709fece909662e5a
John Reyes <johncqr@gmail.com>
2017-11-15 22:42:49 -0800

    temp routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,47 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const db = {
+  'DackJorsey': {
+    points: 0
+  },
+  'ParryLage': {
+    points: 0
+  }
+};
+
+router.get('/:username', function (req, res, next) {
+  const username = req.params.username;
+  if (!(username in db)) {
+    return res.json({ error: 'User not found!' });
+  }
+  return res.json(db[username]);
+});
+
+router.post('/:username', function (req, res, next) {
+  const username = req.params.username
+  if (!(username in db)) {
+    return res.json({ error: 'User not found!' });
+  }
+  const points = req.body.points;
+  db[username].points += points;
+  return res.json(db[username]);
+});
+
+router.delete('/:username', function (req, res, next) {
+  const username = req.params.username
+  if (!(username in db)) {
+    return res.json({ error: 'User not found!' });
+  }
+  db[username].points = 0;
+  return res.json(db[username]);
+})
+
+router.delete('/', function (req, res, next) {
+  for (username in db) {
+    db[username].points = 0;
+  };
+  return res.json(db);
 });
 
 module.exports = router;
